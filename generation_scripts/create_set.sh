cef76fe6d02eb35d803a9b1f22b56bac3f1910d4
Nicholas Barreyre <nicklbarreyre@gmail.com>
2018-07-25 15:11:08 -0300

    Added script to generate set of experiments

---
+++ generation_scripts/create_set.sh
@@ -0,0 +1,56 @@
+#!/bin/bash
+
+# Script to generate a set of experiments on perfRNN
+
+# Usage: ./midi_to_example.sh parent_dir_of_midis desired_output_dir
+# -----------------------------------------------------------------------------
+
+if (( $# < 3 )); then
+	echo "Usage: $0 <num outputs> <run_dir> <output_dir> <config> [<temps to iterate>]"
+	exit 1
+elif (( $1 < 1 )); then
+	echo "Error: Must specify at least 1 output"
+	exit 1
+fi
+
+NUM_OUTPUTS=$1
+RUN_DIR=$2
+OUTPUT_DIR=$3
+CONFIG=$4
+
+shift 4
+
+cd ~/magenta
+source activate magenta
+bazel build --jobs=4 magenta/models/performance_rnn/performance_rnn_generate
+
+# Array of control signals to iterate over
+declare -a signatures
+signatures[0]="[0, 0, 0]"
+signatures[1]="[0, 0, 1]"
+signatures[2]="[0, 1, 0]"
+signatures[3]="[1, 0, 0]"
+signatures[4]="[0, 0, 1.5]"
+signatures[5]="[0, 1.5, 0]"
+signatures[6]="[1.5, 0, 0]"
+signatures[7]="[0, -1, 1]"
+signatures[8]="[0, 1, -1]"
+
+for TEMP in $@
+do
+	for SIG in "${signatures[@]}"
+	do
+		./bazel-bin/magenta/models/performance_rnn/performance_rnn_generate
+		--run_dir=$RUN_DIR \
+		--output_dir=$OUTPUT_DIR \
+		--config=$CONFIG \
+		--num_outputs=$NUM_OUTPUTS \
+		--temperature=$TEMP \
+		--signature_class_histogram=$SIG
+	done
+done 
+
+
+
+
+
