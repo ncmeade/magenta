7c3747f2d0c86d72af8601840920ae4eb5eef7bd
Nick Barreyre <nicklbarreyre@gmail.com>
2019-01-15 11:14:18 -0400

    Temporarily removed augmentation for faster debugging.

--- magenta/models/performance_rnn/performance_rnn_create_dataset.py
+++ magenta/models/performance_rnn/performance_rnn_create_dataset.py
@@ -126,10 +126,10 @@ def get_pipeline(config, min_events, max_events, eval_ratio):
     A pipeline.Pipeline instance.
   """
   # Stretch by -5%, -2.5%, 0%, 2.5%, and 5%.
-  stretch_factors = [0.95, 0.975, 1.0, 1.025, 1.05]
+  stretch_factors = [1.0]
 
   # Transpose no more than a major third.
-  transposition_range = range(-3, 4)
+  transposition_range = range(0, 1)
 
   partitioner = pipelines_common.RandomPartition(
       music_pb2.NoteSequence,
