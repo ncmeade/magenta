fc53c061c6d7960a7f5bc38314a6971abfcbb25c
nick <nick-m@hotmail.ca>
2019-02-10 23:33:04 -0400

    Added density and velocity conditioning

--- magenta/models/performance_rnn/performance_model.py
+++ magenta/models/performance_rnn/performance_model.py
@@ -594,5 +594,27 @@ default_configs = {
             magenta.music.ComposerHistogramPerformanceControlSignal(
                 composers=COMPOSER_SET),
             magenta.music.RelativePositionControlSignal()
+        ]),
+
+    'position_and_composer_conditioned_performance_with_dynamics': PerformanceRnnConfig(
+        magenta.protobuf.generator_pb2.GeneratorDetails(
+            id='position_and_composer_conditioned_performance_with_dynamics',
+            description='Position and composer conditioned performance.'),
+        magenta.music.OneHotEventSequenceEncoderDecoder(
+            magenta.music.PerformanceOneHotEncoding(
+                num_velocity_bins=32)),
+        tf.contrib.training.HParams(
+            batch_size=64,
+            rnn_layer_sizes=[512, 512, 512],
+            dropout_keep_prob=1.0,
+            clip_norm=3,
+            learning_rate=0.001),
+        num_velocity_bins=32,
+        control_signals=[
+            magenta.music.NoteDensityPerformanceControlSignal(
+                window_size_seconds=3.0,
+                density_bin_ranges=[1.0, 2.0, 4.0, 8.0, 16.0, 32.0, 64.0]),
+            magenta.music.VelocityHistogramPerformanceControlSignal(
+                window_size_seconds=5.0)
         ])
 }
